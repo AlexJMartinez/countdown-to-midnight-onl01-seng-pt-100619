5050e40cd701e01380793640b383897e6512a6fc
Alex Martinez <alexandr3martin3z@gmail.com>
2019-10-11 02:18:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,11 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  while countdown_with_sleep > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+end
